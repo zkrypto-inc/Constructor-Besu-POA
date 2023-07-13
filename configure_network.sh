9ccc2c140fc84ccd13c7bd5a3bec02aa682b392e
hardice501 <kr2519@kakao.com>
2023-07-13 18:29:15 +0900

    fix: change script's input parameters and README update

--- configure_network.sh
+++ configure_network.sh
@@ -8,10 +8,8 @@ BESU_EXECUTABLE="besu"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   BESU_EXECUTABLE="/usr/lib/besu-23.4.1/bin/besu"
 fi
-
 RESPONSE=`python3 generation.py -n 4 -c qbft -al 1~4`
-
-./bootnode.sh --CONTAINER_NAME=boot_node
+./bootnode.sh --NODE_NAME=Node-1
 
 sshpass -p "${NODE2_PWD}" scp -r Node-2 genesis.json .env.production "${NODE2}:${NODE2_DIR}"
 sshpass -p "${NODE3_PWD}" scp -r Node-3 genesis.json .env.production "${NODE3}:${NODE3_DIR}"
